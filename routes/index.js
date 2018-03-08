62d882187b16823927cb0405a91bf9fc45f07fd5
Mark Landeryou <marklanderyou@hotmail.com>
2018-03-08 16:55:32 -0500

    updates

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('home', {
     title: 'Video Player Project',
-  message: 'handlebars is awesome' });
+  message: 'Welcome to Our Video Player App' });
 });
 
 module.exports = router;
